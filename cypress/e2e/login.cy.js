badd83448c07e007104fece9ca8c6f0ec0061415
Leonardo Fidelis <leofidelis11@hotmail.com>
2025-07-27 13:44:37 -0300

    adicionando comandos customizados para login, transferencias e commons

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,25 +2,16 @@ describe('Login', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-  it('Login com dados validos deve permitir entrada no sistema', () => {
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').click().type(credenciais.valida.usuario)
-      cy.get('#senha').click().type(credenciais.valida.senha)
-    })
 
-    cy.contains('button', 'Entrar').click()
+  it('Login com dados validos deve permitir entrada no sistema', () => {
+    cy.fazerLoginComCredenciaisValidas()
 
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').click().type(credenciais.invalida.usuario)
-      cy.get('#senha').click().type(credenciais.invalida.senha)
-    })
+    cy.fazerLoginComCredenciaisInvalidas()
 
-    cy.contains('button', 'Entrar').click()
-
-    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+    cy.verificarMensagemNoToast('Erro no login. Tente novamente.')
   })
 })
